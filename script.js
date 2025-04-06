628bc385211f1a554087214a6a469155c38e0625
KamilBCodes <braszkiewiczkamil@gmail.com>
2025-04-06 18:35:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const boxes = document.querySelectorAll('.box');
 
-    boxes.forEach(box => {
-      box.addEventListener('click', () => {
+    const handleBoxClick = (event) => {
+        const box = event.currentTarget;
         const link = box.getAttribute('data-link');
 
-        window.location.href = link;
-      });
+        if (link) {
+            window.location.href = link;
+        } else {
+            console.error('No data-link attribute found for this box.');
+        }
+    };
+
+    boxes.forEach(box => {
+        box.addEventListener('click', handleBoxClick);
     });
-  });
+});
